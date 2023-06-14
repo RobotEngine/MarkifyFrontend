80ae2f84ead27ebb8757705e5427b0689e188184
Exotek <exotekservices@gmail.com>
2023-06-14 01:46:20 -0500

    finished new lesson UI

--- modules/dropdowns/new/blank.js
+++ modules/dropdowns/new/blank.js
@@ -15,14 +15,19 @@ modules["dropdowns/new/blank"] = {
             <button><div class="blankSizeTitle">Custom</div></button>
         </div>
         <div class="blankOptionHolder">
+            <div class="blankCustomSizeHolder">
+                <div class="blankTitle">Page Size</div>
+                <div class="blankNumberHolder"><b>Width</b><div default="8.3" max="50" contenteditable>8.3</div>in</div>
+                <div class="blankNumberHolder"><b>Height</b><div default="11.7" max="50" contenteditable>11.7</div>in</div>
+            </div>
             <div class="blankTitle">Orientation</div>
             <div class="blankSelection" selection>
                 <button selected>Portrait</button>
                 <button>Landscape</button>
             </div>
             <div class="blankTitle">Page Amount</div>
-            <div class="blankEditHolder"><div contenteditable>1</div> pages</div>
-            <button class="blankCreate largeButton">Create Lesson</button>
+            <div class="blankNumberHolder"><div default="1" max="500" nodecimal contenteditable>1</div> pages</div>
+            <button class="blankCreate largeButton" close>Create Lesson</button>
         </div>
     </div>
     `,
@@ -36,21 +41,26 @@ modules["dropdowns/new/blank"] = {
         ".blankSizeHolder button:hover": `outline: solid 3px var(--hover)`,
         ".blackCreationHolder button[selected]": `outline: solid 3px var(--theme); background: var(--hover)`,
         ".blankOptionHolder": `display: flex; flex-direction: column; width: 224px; max-width: 100%; align-items: center`,
-        ".blankSelection": `display: flex; flex-wrap: wrap; width: 100%; margin-bottom: 18px; justify-content: center`,
-        ".blankSelection button": `box-sizing: border-box; display: flex; flex-direction: column; padding: 6px; margin: 6px; justify-content: center; align-items: center; outline: solid 3px var(--secondary); border-radius: 16px; color: var(--theme); font-size: 16px; font-weight: 600`,
+        ".blankCustomSizeHolder": `display: none; width: 100%`,
+        ".blankCustomSizeHolder .blankNumberHolder": `box-sizing: border-box; padding: 0 24px`,
+        ".blankSelection": `display: flex; flex-wrap: wrap; width: 100%; margin-bottom: 14px; justify-content: center`,
+        ".blankSelection button": `box-sizing: border-box; display: flex; flex-direction: column; padding: 6px 10px; margin: 6px; justify-content: center; align-items: center; outline: solid 3px var(--secondary); border-radius: 16px; color: var(--theme); font-size: 16px; font-weight: 600`,
         ".blankSelection button:hover:not([selected])": `outline: solid 3px var(--hover)`,
-        ".blankEditHolder": `display: flex; flex-wrap: wrap; width: 100%; margin-bottom: 18px; justify-content: center; align-items: center`,
-        ".blankOptionHolder div[contenteditable]": `width: fit-content; padding: 4px 6px; margin: 6px 10px; outline: solid 3px var(--secondary); border-radius: 16px; color: var(--theme); font-size: 20px; font-weight: 600; transition: .2s`,
+        ".blankNumberHolder": `display: flex; flex-wrap: wrap; width: 100%; margin-bottom: 14px; justify-content: center; align-items: center`,
+        ".blankNumberHolder b": `margin-right: auto`,
+        ".blankOptionHolder div[contenteditable]": `width: fit-content; max-width: 60px; padding: 4px 6px; margin: 6px 10px; outline: solid 3px var(--secondary); border-radius: 16px; color: var(--theme); font-size: 20px; font-weight: 600; white-space: nowrap; overflow: hidden; transition: .2s`,
         ".blankCreate": `margin: auto 0 22px 0; background: var(--theme); border-radius: 18px; color: #fff`
     },
     js: function (frame) {
-        frame.querySelector(".blackCreationHolder").addEventListener("click", function(event) {
+        let frameCreationHolder = frame.querySelector(".blackCreationHolder");
+        let customSizeHolder = frame.querySelector(".blankCustomSizeHolder");
+        frameCreationHolder.addEventListener("click", function(event) {
             let element = event.target;
             if (element == null) {
                 return;
             }
-            element = element.closest("button") || element;
-            if (element.parentElement.hasAttribute("selection") == false) {
+            element = element.closest("button");
+            if (element == null || element.parentElement.hasAttribute("selection") == false) {
                 return;
             }
             let lastSelected = element.parentElement.querySelector("button[selected]");
@@ -58,32 +68,58 @@ modules["dropdowns/new/blank"] = {
                 lastSelected.removeAttribute("selected");
             }
             element.setAttribute("selected", "");
-        });
-        let textInputBox = frame.querySelector(".blankEditHolder div");
-        textInputBox.addEventListener("mousedown", function() {
-            textInputBox.textContent = "";
-        });
-        async function textBoxError() {
-            textInputBox.style.outlineColor = "var(--error)";
-            textInputBox.style.color = "var(--error)";
-            await sleep(200);
-            textInputBox.style.outlineColor = "var(--theme)";
-            textInputBox.style.color = "var(--theme)";
-        }
-        textInputBox.addEventListener("keydown", function(event) {
-            if (String.fromCharCode(event.keyCode).match(/(\w|\s)/g) || event.key.length == 1) {
-                if (parseInt(event.key) != event.key) {
-                    event.preventDefault();
-                    textBoxError();
-                } else if (parseInt(textInputBox.textContent + event.key) > 500) {
-                    event.preventDefault();
-                    textBoxError();
+            let title = element.querySelector(".blankSizeTitle");
+            if (title) {
+                if (title.textContent == "Custom") {
+                    customSizeHolder.style.display = "block";
+                } else {
+                    customSizeHolder.style.display = "none";
                 }
             }
         });
-        textInputBox.addEventListener("focusout", function() {
-            if (textInputBox.textContent == "") {
-                textInputBox.textContent = 1;
+        frameCreationHolder.addEventListener("mousedown", function(event) {
+            let textBox = event.target.closest(".blankNumberHolder div");
+            if (textBox == null) {
+                return;
+            }
+            textBox.textContent = "";
+        });
+        async function textBoxError(box, error) {
+            box.style.outlineColor = "var(--error)";
+            box.style.color = "var(--error)";
+            await sleep(200);
+            box.style.outlineColor = "var(--secondary)";
+            box.style.color = "var(--secondary)";
+        }
+        frameCreationHolder.addEventListener("keydown", function(event) {
+            let textBox = event.target.closest(".blankNumberHolder div");
+            if (textBox == null) {
+                return;
+            }
+            if (String.fromCharCode(event.keyCode).match(/(\w|\s)/g) || event.key.length == 1) {
+                let textInt = parseFloat(textBox.textContent + event.key);
+                if (parseInt(event.key) != event.key && (event.keyCode != 190 || textBox.hasAttribute("nodecimal"))) {
+                    event.preventDefault();
+                    textBoxError(textBox, "Input must be a number.");
+                } else if (textInt > parseFloat(textBox.getAttribute("max"))) {
+                    event.preventDefault();
+                    textBoxError(textBox, "Input must be under " + textBox.getAttribute("max"));
+                } else if (textInt < 1) {
+                    event.preventDefault();
+                    textBoxError(textBox, "Input must be greater than 1");
+                }
+            }
+        });
+        frameCreationHolder.addEventListener("focusout", function(event) {
+            let textBox = event.target.closest(".blankNumberHolder div");
+            if (textBox == null) {
+                return;
+            }
+            let textInt = parseFloat(textBox.textContent) || 0;
+            if (textInt > parseFloat(textBox.getAttribute("max"))) {
+                textBox.textContent = textBox.getAttribute("max");
+            } else if (textInt < 1) {
+                textBox.textContent = 1;
             }
         });
     }

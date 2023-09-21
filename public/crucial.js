07da57f6930e5de8d74d274c0c3412b548032baf
Exotek <exotekservices@gmail.com>
2023-09-21 00:28:28 -0500

    added small 2px shrink on border

--- public/crucial.js
+++ public/crucial.js
@@ -1036,7 +1036,7 @@ addCSS({
   "[hidden]": `pointer-events: none !important; opacity: 0 !important`,
   ".largeButton, .border": `--themeColor: var(--secondary); --themeColor2: var(--hover); --borderRadius: 0px; --borderColor: var(--themeColor); --borderWidth: 0px; --outline: solid var(--borderWidth) var(--borderColor); --transition: .1s; position: relative; border-radius: var(--borderRadius)`,
   ".largeButton": `display: flex; padding: 6px 14px; --borderWidth: 4px; align-items: center; font-size: 20px; font-weight: 700; transition: .1s`,
-  ".largeButton:after, .border:after": `position: absolute; display: inline-block; width: 100%; height: 100%; left: 50%; top: 50%; transform: translate(-50%, -50%); border-radius: calc(var(--borderRadius) + var(--borderWidth)*2); content: ""; pointer-events: none; border: var(--outline); transition: var(--transition)`,
+  ".largeButton:after, .border:after": `position: absolute; display: inline-block; width: calc(100% - 2px); height: calc(100% - 2px); left: 50%; top: 50%; transform: translate(-50%, -50%); border-radius: calc(var(--borderRadius) + var(--borderWidth)*2); content: ""; pointer-events: none; border: var(--outline); transition: var(--transition)`,
   ".buttonAnim": `--borderWidth: 0px`,
   ".buttonAnim:hover": `background: var(--hover)`,
   ".buttonAnim:active": `background: var(--pageColor); --borderWidth: 4px; --borderColor: var(--hover)`,

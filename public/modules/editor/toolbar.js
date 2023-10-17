684087c7c52dc25ccb7f526416d9c6375be4a0b7
Exotek <exotekservices@gmail.com>
2023-10-16 21:37:01 -0500

    added debug prompt

--- public/modules/editor/toolbar.js
+++ public/modules/editor/toolbar.js
@@ -906,7 +906,8 @@ modules["pages/editor/toolbar/eraser"] = {
             }
           }
         }
-        
+
+        prompt(x0 + "; " + y0 + "; " + x1 + "; " + y1)
         if (x0 === x1 && y0 === y1) {
           break;
         }

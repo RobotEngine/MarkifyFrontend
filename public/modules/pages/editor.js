24fa842985cfdf86bb835c549b5f3e197d010291
Exotek <exotekservices@gmail.com>
2024-01-05 08:29:04 -0600

    fixed pdf loading

--- public/modules/pages/editor.js
+++ public/modules/pages/editor.js
@@ -1236,8 +1236,8 @@ modules["pages/editor"] = {
 
                 canvas.width = viewport.width;
                 canvas.height = viewport.height;
-                pageElem.style.width = viewport.width + "px";
-                pageElem.style.height = viewport.height + "px";
+                //pageElem.style.width = viewport.width + "px";
+                //pageElem.style.height = viewport.height + "px";
 
                 pageRender.render({
                   canvasContext: context,

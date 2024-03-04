1fc0cb74958513db7a6a5e6953e712e8d30cfefc
Exotek <exotekservices@gmail.com>
2024-03-03 19:11:27 -0600

    added webkit blur

--- public/crucial.js
+++ public/crucial.js
@@ -2,7 +2,7 @@ let serverURL = "https://markify.exotek.co/api/";
 //let serverURL = "http://localhost:3000/api/";
 let assetURL = "https://markifyapp.s3.amazonaws.com/";
 
-const version = "0.2.3"; // Big Update . Small Feature Release . Bug Fix
+const version = "0.2.4"; // Big Update . Small Feature Release . Bug Fix
 
 const socket = new SimpleSocket({
   project_id: "62088fbdfc22489578e94822",

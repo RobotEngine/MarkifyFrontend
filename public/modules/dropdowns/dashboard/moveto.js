9ecab218a5e1f438f796fc789cfa0e3e25ae334c
Exotek <exotekservices@gmail.com>
2024-03-27 17:01:29 -0500

    finished more folder ui

--- public/modules/dropdowns/dashboard/moveto.js
+++ public/modules/dropdowns/dashboard/moveto.js
@@ -4,36 +4,41 @@ modules["dropdowns/dashboard/moveto"] = {
     <div class="dTileDropFolderHolder">
       <div class="dTileDropFolderParent">
         <button class="dTileDropFolder">
+        <div select></div>
           <svg folder viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg"> <mask id="mask0_1422_21" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="256" height="256"> <rect width="256" height="256" fill="white"/> </mask> <g mask="url(#mask0_1422_21)"> <path d="M223 178V101.747C223 86.8351 210.912 74.7468 196 74.7468H121V73C121 60.2974 110.703 50 98 50H56C43.2974 50 33 60.2975 33 73V178C33 192.912 45.0883 205 60 205H196C210.912 205 223 192.912 223 178Z" stroke="var(--themeColor)" stroke-width="30"/> </g> </svg>
-          <div>Untitled Folder</div>
+          <div name>Untitled Folder</div>
           <div arrow><svg arrow viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg"> <mask id="mask0_1455_45" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="256" height="256"> <rect width="256" height="256" transform="matrix(1 0 0 -1 0 256)" fill="#C4C4C4"/> </mask> <g mask="url(#mask0_1455_45)"> <path d="M85.1472 213L170 128.147" stroke="var(--themeColor)" stroke-width="48" stroke-linecap="round"/> <path d="M85.1472 43.2942L170 128.147" stroke="var(--themeColor)" stroke-width="48" stroke-linecap="round"/> </g> </svg></div>
         </button>
       </div>
       <div class="dTileDropFolderParent">
-        <button class="dTileDropFolder">
+        <button class="dTileDropFolder" style="--themeColor: var(--green)">
+          <div select></div>
           <svg folder viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg"> <mask id="mask0_1422_21" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="256" height="256"> <rect width="256" height="256" fill="white"/> </mask> <g mask="url(#mask0_1422_21)"> <path d="M223 178V101.747C223 86.8351 210.912 74.7468 196 74.7468H121V73C121 60.2974 110.703 50 98 50H56C43.2974 50 33 60.2975 33 73V178C33 192.912 45.0883 205 60 205H196C210.912 205 223 192.912 223 178Z" stroke="var(--themeColor)" stroke-width="30"/> </g> </svg>
-          <div>Untitled Folder</div>
-          <div arrow><svg viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg"> <mask id="mask0_1455_45" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="256" height="256"> <rect width="256" height="256" transform="matrix(1 0 0 -1 0 256)" fill="#C4C4C4"/> </mask> <g mask="url(#mask0_1455_45)"> <path d="M85.1472 213L170 128.147" stroke="var(--themeColor)" stroke-width="48" stroke-linecap="round"/> <path d="M85.1472 43.2942L170 128.147" stroke="var(--themeColor)" stroke-width="48" stroke-linecap="round"/> </g> </svg></div>
+          <div name>Untitled Folder</div>
+          <div arrow style="transform: rotate(90deg)"><svg viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg"> <mask id="mask0_1455_45" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="256" height="256"> <rect width="256" height="256" transform="matrix(1 0 0 -1 0 256)" fill="#C4C4C4"/> </mask> <g mask="url(#mask0_1455_45)"> <path d="M85.1472 213L170 128.147" stroke="var(--themeColor)" stroke-width="48" stroke-linecap="round"/> <path d="M85.1472 43.2942L170 128.147" stroke="var(--themeColor)" stroke-width="48" stroke-linecap="round"/> </g> </svg></div>
         </button>
         <div class="dTileDropFolderParent" child>
-          <button class="dTileDropFolder">
+          <button class="dTileDropFolder" style="--themeColor: var(--yellow)">
+            <div select></div>
             <svg folder viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg"> <mask id="mask0_1422_21" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="256" height="256"> <rect width="256" height="256" fill="white"/> </mask> <g mask="url(#mask0_1422_21)"> <path d="M223 178V101.747C223 86.8351 210.912 74.7468 196 74.7468H121V73C121 60.2974 110.703 50 98 50H56C43.2974 50 33 60.2975 33 73V178C33 192.912 45.0883 205 60 205H196C210.912 205 223 192.912 223 178Z" stroke="var(--themeColor)" stroke-width="30"/> </g> </svg>
-            <div>Long Name Untitled Folder</div>
-            <div arrow><svg viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg"> <mask id="mask0_1455_45" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="256" height="256"> <rect width="256" height="256" transform="matrix(1 0 0 -1 0 256)" fill="#C4C4C4"/> </mask> <g mask="url(#mask0_1455_45)"> <path d="M85.1472 213L170 128.147" stroke="var(--themeColor)" stroke-width="48" stroke-linecap="round"/> <path d="M85.1472 43.2942L170 128.147" stroke="var(--themeColor)" stroke-width="48" stroke-linecap="round"/> </g> </svg></div>
+            <div name>Long Name Untitled Folder</div>
+            <div arrow style="transform: rotate(90deg)"><svg viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg"> <mask id="mask0_1455_45" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="256" height="256"> <rect width="256" height="256" transform="matrix(1 0 0 -1 0 256)" fill="#C4C4C4"/> </mask> <g mask="url(#mask0_1455_45)"> <path d="M85.1472 213L170 128.147" stroke="var(--themeColor)" stroke-width="48" stroke-linecap="round"/> <path d="M85.1472 43.2942L170 128.147" stroke="var(--themeColor)" stroke-width="48" stroke-linecap="round"/> </g> </svg></div>
           </button>
           <div class="dTileDropFolderParent" child>
             <button class="dTileDropFolder">
+              <div select></div>
               <svg folder viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg"> <mask id="mask0_1422_21" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="256" height="256"> <rect width="256" height="256" fill="white"/> </mask> <g mask="url(#mask0_1422_21)"> <path d="M223 178V101.747C223 86.8351 210.912 74.7468 196 74.7468H121V73C121 60.2974 110.703 50 98 50H56C43.2974 50 33 60.2975 33 73V178C33 192.912 45.0883 205 60 205H196C210.912 205 223 192.912 223 178Z" stroke="var(--themeColor)" stroke-width="30"/> </g> </svg>
-              <div>Untitled Folder</div>
+              <div name>Untitled Folder</div>
               <div arrow><svg viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg"> <mask id="mask0_1455_45" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="256" height="256"> <rect width="256" height="256" transform="matrix(1 0 0 -1 0 256)" fill="#C4C4C4"/> </mask> <g mask="url(#mask0_1455_45)"> <path d="M85.1472 213L170 128.147" stroke="var(--themeColor)" stroke-width="48" stroke-linecap="round"/> <path d="M85.1472 43.2942L170 128.147" stroke="var(--themeColor)" stroke-width="48" stroke-linecap="round"/> </g> </svg></div>
             </button>
           </div>
         </div>
       </div>
       <div class="dTileDropFolderParent">
-        <button class="dTileDropFolder">
+        <button class="dTileDropFolder" style="--themeColor: var(--error)">
+          <div select></div>
           <svg folder viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg"> <mask id="mask0_1422_21" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="256" height="256"> <rect width="256" height="256" fill="white"/> </mask> <g mask="url(#mask0_1422_21)"> <path d="M223 178V101.747C223 86.8351 210.912 74.7468 196 74.7468H121V73C121 60.2974 110.703 50 98 50H56C43.2974 50 33 60.2975 33 73V178C33 192.912 45.0883 205 60 205H196C210.912 205 223 192.912 223 178Z" stroke="var(--themeColor)" stroke-width="30"/> </g> </svg>
-          <div>Untitled Folder</div>
+          <div name>Untitled Folder</div>
           <div arrow><svg arrow viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg"> <mask id="mask0_1455_45" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="256" height="256"> <rect width="256" height="256" transform="matrix(1 0 0 -1 0 256)" fill="#C4C4C4"/> </mask> <g mask="url(#mask0_1455_45)"> <path d="M85.1472 213L170 128.147" stroke="var(--themeColor)" stroke-width="48" stroke-linecap="round"/> <path d="M85.1472 43.2942L170 128.147" stroke="var(--themeColor)" stroke-width="48" stroke-linecap="round"/> </g> </svg></div>
         </button>
       </div>
@@ -46,13 +51,14 @@ modules["dropdowns/dashboard/moveto"] = {
   `,
   css: {
     ".dTileDropFolderFrame": `width: fit-content`,
-    ".dTileDropFolderHolder": `display: flex; flex-direction: column; width: max-content; align-items: flex-start`,
+    ".dTileDropFolderHolder": `display: flex; flex-direction: column; width: max-content; min-width: 100%; align-items: flex-start`,
     ".dTileDropFolderParent": `width: -webkit-fill-available`,
     ".dTileDropFolderParent[child]": `padding-left: 10px`,
-    ".dTileDropFolder": `--themeColor: var(--theme); display: flex; padding: 4px 0px 4px 4px; width: 100%; margin-bottom: 6px; align-items: center; border-radius: 24px`,
-    ".dTileDropFolder:hover": `background: var(--hover)`,
+    ".dTileDropFolder": `--themeColor: var(--theme); position: relative; display: flex; padding: 4px; width: 100%; margin-bottom: 6px; align-items: center`,
     ".dTileDropFolder svg[folder]": `width: 32px; height: 32px; margin-left: 4px`,
-    ".dTileDropFolder div": `margin: 0 4px; color: var(--textColor); font-size: 18px; font-weight: 600`,
+    ".dTileDropFolder div[select]": `position: absolute; width: 100%; height: 100%; left: 0px; top: 0px; background: var(--themeColor); border-radius: 24px; opacity: 0; transition: .1s`,
+    ".dTileDropFolder:hover div[select]": `opacity: .2`,
+    ".dTileDropFolder div[name]": `margin: 0 4px; color: var(--textColor); font-size: 18px; font-weight: 600`,
     ".dTileDropFolder div[arrow]": `position: sticky; display: flex; width: 32px; height: 32px; right: -2px; margin-left: auto; justify-content: center; align-items: center; background: rgba(var(--background), .7); backdrop-filter: blur(4px); border-radius: 16px`,
     ".dTileDropFolder div[arrow] svg": `width: 24px; height: 24px`,
     ".dTileDropFolderActions": `position: sticky; display: flex; flex-wrap: wrap; max-width: calc(var(--dropdownWidth) - 36px); padding: 8px; margin: 4px; gap: 24px; left: 4px; bottom: 4px; justify-content: space-between; align-items: center; background: rgba(var(--background), .7); backdrop-filter: blur(4px); border-radius: 27px`,

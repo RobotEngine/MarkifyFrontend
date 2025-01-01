1a8a496d77c4d2f7d0dee84272a1f58c71772686
Exotek <exotekservices@gmail.com>
2025-01-01 11:16:55 -0600

    created editor module

--- public/modules/pages/lesson.js
+++ public/modules/pages/lesson.js
@@ -11,31 +11,6 @@ modules["pages/lesson"] = class {
     ".lPage": `display: flex; width: 100%; height: 100%; max-width: 1565px; box-shadow: var(--darkShadow); border-radius: 12px; overflow: hidden`
   };
 
-  // PIPELINE : Distributes events across various modules and services:
-  pipeline = {};
-  pipelineSubs = {};
-  pipePublish = (event, data) => {
-    let subscribes = this.pipeline[event] ?? {};
-    let subKeys = Object.keys(subscribes);
-    for (let i = 0; i < subKeys.length; i++) {
-      subscribes[subKeys[i]](data);
-    }
-  }
-  pipeSubscribe = (id, event, callback) => {
-    if (this.pipelineSubs[id] != null) {
-      this.pipeUnsubscribe(id);
-    }
-
-    this.pipeline[event] = this.pipeline[event] ?? {};
-    this.pipeline[event][id] = callback;
-
-    this.pipelineSubs[id] = event;
-  }
-  pipeUnsubscribe = (id) => {
-    delete this.pipeline[this.pipelineSubs[id]][id];
-    delete this.pipelineSubs[id];
-  }
-
   // LESSON PAGE : Loads lessons, members, and configs before creating editor modules:
   js = async (page, joinData) => {
     this.id = getParam("lesson") ?? "";
@@ -76,5 +51,48 @@ modules["pages/lesson"] = class {
     this.sessionID = body.session._id;
     this.sessionToken = body.session.token;
     this.session = this.sessionID + ";" + this.sessionToken;
+  }
+}
+
+modules["pages/lesson/editor"] = class {
+  title = "Lesson";
+  preJs = () => {
+
+  }
+  html = `<div class="lPageHolder">
+    <div class="lPage"></div>
+  </div>`;
+  css = {
+    ".lPageHolder": `position: fixed; display: flex; box-sizing: border-box; width: 100%; height: 100vh; padding: 8px; left: 0px; top: 0px; justify-content: center`,
+    ".lPage": `display: flex; width: 100%; height: 100%; max-width: 1565px; box-shadow: var(--darkShadow); border-radius: 12px; overflow: hidden`
+  };
+
+  // PIPELINE : Distributes events across various modules and services:
+  pipeline = {};
+  pipelineSubs = {};
+  pipePublish = (event, data) => {
+    let subscribes = this.pipeline[event] ?? {};
+    let subKeys = Object.keys(subscribes);
+    for (let i = 0; i < subKeys.length; i++) {
+      subscribes[subKeys[i]](data);
+    }
+  }
+  pipeSubscribe = (id, event, callback) => {
+    if (this.pipelineSubs[id] != null) {
+      this.pipeUnsubscribe(id);
+    }
+
+    this.pipeline[event] = this.pipeline[event] ?? {};
+    this.pipeline[event][id] = callback;
+
+    this.pipelineSubs[id] = event;
+  }
+  pipeUnsubscribe = (id) => {
+    delete this.pipeline[this.pipelineSubs[id]][id];
+    delete this.pipelineSubs[id];
+  }
+
+  js = async (frame, extra) => {
+
   }
 }

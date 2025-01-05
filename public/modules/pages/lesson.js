8e9688c7e0429d2b26064e9d9203a48130bced2c
Exotek <exotekservices@gmail.com>
2025-01-05 01:12:53 -0600

    added interface buttons to top bar

--- public/modules/pages/lesson.js
+++ public/modules/pages/lesson.js
@@ -103,36 +103,85 @@ modules["pages/lesson"] = class {
 
 modules["pages/lesson/board"] = class {
   html = `<div class="eInterface eCustomScroll">
-    <div class="eTop">
-      <button class="eTopScroll" left style="left: 8px"><img src="./images/editor/top/leftarrow.svg"></button>
-      <button class="eTopScroll" right style="right: 8px"><img src="./images/editor/top/rightarrow.svg"></button>
-      <div class="eTopSection" left></div>
-      <div class="eTopSection" right></div>
+    <div class="eTopHolder">
+      <button class="eTopScroll" left style="left: 8px"><img src="./images/editor/top/leftarrow.svg" /></button>
+      <button class="eTopScroll" right style="right: 8px"><img src="./images/editor/top/rightarrow.svg" /></button>
+      <div class="eTop">
+        <div class="eTopSection" left>
+          <a class="eLogo" href="#dashboard"><img src="./images/icon.svg" /></a>
+          <div class="eFileNameHolder border"><div class="eFileName" spellcheck="false" onpaste="clipBoardRead(event)">My Lovely Lesson</div></div>
+          <button class="eFileDropdown">File</button>
+          <div class="eTopDivider"></div>
+          <button class="eSaveProgress eUndo" disabled><img draggable="false" src="./images/tooltips/progress/undo.svg" /></button>
+          <button class="eSaveProgress eRedo" disabled><img draggable="false" src="./images/tooltips/progress/redo.svg" /></button>
+        </div>
+        <div class="eTopSection" right>
+          <button class="eMembers"><span class="eMemberHandCount" title="Number of hands raised."></span><span class="eMemberIdleCount" title="Number of idle members."></span><span class="eMemberCount" title="Number of members."></span>Members</button>
+          <button class="eEndSession" title="End Session | Disable all editing access making everyone a viewer."><img src="./images/editor/share/endeditors.svg" /></button>
+          <button class="eShare">Share</button>
+          <button class="eMemberOptions" title="Member Options | Configure various member settings and available tools."><img src="./images/editor/share/setting.svg" /></button>
+          <button class="eSharePin"></button>
+          <div class="eTopDivider"></div>
+          <button class="eZoom"><span class="eZoomBox">100</span>%</button>
+          <button class="eAccount"><img src="./images/profiles/default.svg" accountimage /><div accountuser></div></button>
+          <button class="eLogin">Login</button>
+        </div>
+      </div>
     </div>
   </div>
-  <div class="eContentHolder eCustomScroll">
-
-  </div>
+  <div class="eContentHolder eCustomScroll"></div>
   `;
   css = {
     ".eCustomScroll::-webkit-scrollbar": `width: 16px; background: var(--scrollGray)`,
+    ".eCustomScroll::-webkit-scrollbar-corner": `background: var(--scrollGray)`,
     ".eCustomScroll::-webkit-scrollbar-thumb": `border: 4px solid var(--scrollGray); background: var(--gray); border-radius: 8px`,
     ".eCustomScroll::-webkit-scrollbar-thumb:active": `background: var(--activeGray)`,
     ".eInterface": `position: absolute; width: 100%; height: 100%; left: 0px; top: 0px; visibility: hidden; pointer-events: none; overflow: scroll; z-index: 2`,
     ".eContentHolder": `position: relative; width: 100%; height: 100%; overflow: scroll; z-index: 1`,
     ".eContentHolder .content": `width: 5000px; height: 5000px`, // Just a test
-
-    ".eTop": `position: absolute; display: flex; width: 100%; padding-bottom: 8px; left: 0px; top: 0px; justify-content: space-between; visibility: visible; overflow-x: auto; scrollbar-width: none`,
-    ".eTop[scroll]": `padding-bottom: 0px !important; background: var(--pageColor); box-shadow: var(--lightShadow); pointer-events: all`,
+    
+    ".eTopHolder": `position: relative; width: 100%; height: 50px; visibility: visible`,
+    ".eTop": `position: absolute; display: flex; width: 100%; padding-bottom: 8px; left: 0px; top: 0px; justify-content: space-between; overflow-x: auto; scrollbar-width: none`,
+    ".eTopHolder[scroll] .eTop": `padding-bottom: 0px !important; background: var(--pageColor); box-shadow: var(--lightShadow); pointer-events: all`,
     ".eTop::-webkit-scrollbar": `display: none`,
-    ".eTopScroll": `position: absolute; display: flex; width: 36px; height: 36px; top: 50%; transform: translateY(-50%); background: rgba(180, 218, 253, .75); opacity: 0; backdrop-filter: blur(2px); pointer-events: none; border-radius: 18px; justify-content: center; align-items: center; z-index: 200`,
-    ".eTop[scroll] .eTopScroll": `opacity: 1 !important; pointer-events: all`,
+    ".eTopScroll": `position: absolute; display: flex; width: 36px; height: 36px; top: 50%; transform: translateY(-50%); background: rgba(180, 218, 253, .75); opacity: 0; backdrop-filter: blur(2px); border-radius: 18px; justify-content: center; align-items: center; z-index: 200`,
+    ".eTopHolder[scroll] .eTopScroll": `opacity: 1 !important; pointer-events: all`,
     ".eTopScroll img": `width: 22px`,
     ".eTopScroll:active": `transform: translateY(-50%) scale(.85) !important`,
     ".eTopSection": `display: flex; box-sizing: border-box; height: 50px; padding: 6px; flex-shrink: 0; align-items: center; background: var(--pageColor); box-shadow: var(--lightShadow); pointer-events: all`,
-    ".eTop[scroll] .eTopSection": `box-shadow: unset !important`,
-    ".eTopSection[left]": `border-bottom-right-radius: 12px; width: 400px`,
-    ".eTopSection[right]": `border-bottom-left-radius: 12px; width: 400px`
+    ".eTopHolder[scroll] .eTopSection": `box-shadow: unset !important`,
+    ".eTopSection[left]": `border-bottom-right-radius: 12px`,
+    ".eTopSection[right]": `border-bottom-left-radius: 12px`,
+
+    ".eLogo": `display: flex; width: 38px; height: 38px; padding: 0; margin-right: 4px; user-select: none; justify-content: center; align-items: center; border-radius: 6px`,
+    ".eLogo img": `width: 34px; height: 34px`,
+    //".eLogo:hover": `background: var(--hover)`,
+    ".eFileNameHolder": `margin: 0 4px; --borderRadius: 4px; --borderColor: var(--secondary); --borderWidth: 0px; --transition: .05s`,
+    ".eFileName": `max-width: 350px; padding: 0px; outline: unset; font-size: 20px; white-space: nowrap; overflow-x: hidden; text-overflow: ellipsis; scrollbar-width: none`,
+    ".eFileName::-webkit-scrollbar": `display: none`,
+    //".eFileName:focus": `--borderWidth: 4px`,
+    ".eFileDropdown": `padding: 6px 10px; height: 32px; margin: 0 4px; background: var(--lightGray); border-radius: 16px; font-size: 16px; font-weight: 600`,
+    ".eTopDivider": `width: 4px; height: 32px; margin: 0 4px; background: var(--lightGray); border-radius: 2px`,
+    ".eSaveProgress": `display: flex; width: 32px; height: 32px; padding: 0; align-items: center; overflow: hidden; background: var(--lightGray)`,
+    ".eSaveProgress img": `width: 24px; height: 24px; margin: 2px`,
+    ".eUndo": `margin: 0 2px 0 4px; justify-content: end; border-radius: 16px 0 0 16px`,
+    ".eRedo": `margin: 0 4px 0 2px; justify-content: start; border-radius: 0 16px 16px 0`,
+
+    ".eMembers": `display: flex; height: 32px; padding: 6px 10px; margin: 0 4px; background: var(--hover); border-radius: 16px; align-items: center; font-size: 16px; font-weight: 600`,
+    ".eMemberCount": `display: none; padding: 2px 6px; margin-right: 5px; background: #fff; border-radius: 12px; color: var(--theme); font-weight: 700`,
+    ".eMemberHandCount": `display: none; padding: 2px 6px; margin-right: 5px; background: #fff; border-radius: 12px; color: var(--green); font-weight: 700`,
+    ".eMemberIdleCount": `display: none; padding: 2px 6px; margin-right: 5px; background: #fff; border-radius: 12px; color: var(--yellow); font-weight: 700`,
+    ".eEndSession": `display: none; width: 32px; height: 32px; padding: 0px; margin: 0 4px; background: var(--error); border-radius: 16px; justify-content: center; align-items: center; color: #fff; font-size: 16px; font-weight: 600`,
+    ".eEndSession img": `width: 28px; height: 28px`,
+    ".eShare": `height: 32px; padding: 6px 10px; margin: 0 4px; background: var(--theme); border-radius: 16px; color: #fff; font-size: 16px; font-weight: 600`,
+    ".eMemberOptions": `display: flex; width: 32px; height: 32px; padding: 0px; margin: 0 4px; background: var(--lightGray); border-radius: 16px; justify-content: center; align-items: center; color: #fff; font-size: 16px; font-weight: 600`,
+    ".eMemberOptions img": `width: 32px; height: 32px`,
+    ".eSharePin": `display: none; height: 32px; padding: 6px 10px; margin: 0 4px; background: var(--lightGray); border-radius: 16px; font-size: 16px; font-weight: 600`,
+    ".eZoom": `height: 32px; padding: 6px 10px; margin: 0 4px; background: var(--lightGray); border-radius: 16px; font-size: 16px; font-weight: 600`,
+    ".eAccount": `height: 32px; padding: 0; width: 31px; height: 31px; margin-left: 4px; border-radius: 16px; overflow: hidden`,
+    ".eAccount img": `width: 100%; height: 100%; object-fit: cover`,
+    ".eLogin": `height: 32px; display: none; padding: 6px 10px; margin-left: 4px; background: var(--secondary); border-radius: 16px; color: #fff; font-size: 16px; font-weight: 600`,
+
   }; //".": ``,
   js = async (frame, extra) => {
     frame.style.position = "relative";

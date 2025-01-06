b80873a9bc074a00c293c91da3dccfc019a04654
Exotek <exotekservices@gmail.com>
2025-01-06 00:53:38 -0600

    added side toolbar and bottom bar holder

--- public/modules/pages/lesson.js
+++ public/modules/pages/lesson.js
@@ -114,6 +114,15 @@ modules["pages/lesson/board"] = class {
           <div class="eTopDivider"></div>
           <button class="eSaveProgress eUndo" disabled><img draggable="false" src="./images/tooltips/progress/undo.svg" /></button>
           <button class="eSaveProgress eRedo" disabled><img draggable="false" src="./images/tooltips/progress/redo.svg" /></button>
+          <div class="eStatus">
+            <div class="eStatusStrength" full>
+              <svg saved width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="15.2344" cy="15.2344" r="12.4219" stroke="#48A7FF" stroke-width="3.75"/>
+                <path d="M9.4375 15.9905L13.1524 19.7054C13.8847 20.4377 15.0719 20.4377 15.8041 19.7054L25.6399 9.86957" stroke="white" stroke-width="7.5" stroke-linecap="round"/>
+                <path d="M9.4375 15.793L13.9458 20.061C14.3073 20.4033 14.8733 20.4033 15.2348 20.061L26 9.86957" stroke="#48A7FF" stroke-width="3.75" stroke-linecap="round"/>
+              </svg>
+            </div>
+          </div>
         </div>
         <div class="eTopSection" right>
           <button class="eMembers"><span class="eMemberHandCount" title="Number of hands raised."></span><span class="eMemberIdleCount" title="Number of idle members."></span><span class="eMemberCount" title="Number of members."></span>Members</button>
@@ -128,6 +137,27 @@ modules["pages/lesson/board"] = class {
         </div>
       </div>
     </div>
+    <div class="eToolbarHolder">
+      <div class="eToolbar"></div>
+      <div class="eViewerActions">
+        <button hidden class="eHandRaise largeButton" tool><img src="./images/editor/actions/raisehand.svg" /></button>
+      </div>
+    </div>
+    <div class="eBottomHolder">
+      <div class="eBottom">
+        <div class="eBottomSection" left>
+          <div class="eObserve">
+            <div>Observing <b></b></div>
+            <button><img src="./images/tooltips/exit.svg" /></button>
+          </div>
+        </div>
+        <div class="eBottomSection" right>
+          <button class="ePageNav" down><img src="./images/editor/bottom/uparrow.svg" /></button>
+          <div class="eCurrentPage border" contenteditable></div>
+          <button class="ePageNav" up><img src="./images/editor/bottom/downarrow.svg" /></button>
+        </div>
+      </div>
+    </div>
   </div>
   <div class="eContentHolder eCustomScroll"></div>
   `;
@@ -136,7 +166,7 @@ modules["pages/lesson/board"] = class {
     ".eCustomScroll::-webkit-scrollbar-corner": `background: var(--scrollGray)`,
     ".eCustomScroll::-webkit-scrollbar-thumb": `border: 4px solid var(--scrollGray); background: var(--gray); border-radius: 8px`,
     ".eCustomScroll::-webkit-scrollbar-thumb:active": `background: var(--activeGray)`,
-    ".eInterface": `position: absolute; width: 100%; height: 100%; left: 0px; top: 0px; visibility: hidden; pointer-events: none; overflow: scroll; z-index: 2`,
+    ".eInterface": `position: absolute; display: flex; flex-direction: column; width: 100%; height: 100%; left: 0px; top: 0px; visibility: hidden; pointer-events: none; overflow: scroll; z-index: 2`,
     ".eContentHolder": `position: relative; width: 100%; height: 100%; overflow: scroll; z-index: 1`,
     ".eContentHolder .content": `width: 5000px; height: 5000px`, // Just a test
     
@@ -154,7 +184,7 @@ modules["pages/lesson/board"] = class {
     ".eTopSection[right]": `border-bottom-left-radius: 12px`,
 
     ".eLogo": `display: flex; width: 38px; height: 38px; padding: 0; margin-right: 4px; user-select: none; justify-content: center; align-items: center; border-radius: 6px`,
-    ".eLogo img": `width: 34px; height: 34px`,
+    ".eLogo img": `width: 32px; height: 32px`,
     //".eLogo:hover": `background: var(--hover)`,
     ".eFileNameHolder": `margin: 0 4px; --borderRadius: 4px; --borderColor: var(--secondary); --borderWidth: 0px; --transition: .05s`,
     ".eFileName": `max-width: 350px; padding: 0px; outline: unset; font-size: 20px; white-space: nowrap; overflow-x: hidden; text-overflow: ellipsis; scrollbar-width: none`,
@@ -166,6 +196,8 @@ modules["pages/lesson/board"] = class {
     ".eSaveProgress img": `width: 24px; height: 24px; margin: 2px`,
     ".eUndo": `margin: 0 2px 0 4px; justify-content: end; border-radius: 16px 0 0 16px`,
     ".eRedo": `margin: 0 4px 0 2px; justify-content: start; border-radius: 0 16px 16px 0`,
+    ".eStatus": `display: flex; width: 32px; height: 32px; margin: 4px; justify-content: center; align-items: center`,
+    ".eStatusStrength": `display: flex; width: 100%; height: 100%; justify-content: center; align-items: center`,
 
     ".eMembers": `display: flex; height: 32px; padding: 6px 10px; margin: 0 4px; background: var(--hover); border-radius: 16px; align-items: center; font-size: 16px; font-weight: 600`,
     ".eMemberCount": `display: none; padding: 2px 6px; margin-right: 5px; background: #fff; border-radius: 12px; color: var(--theme); font-weight: 700`,
@@ -182,6 +214,28 @@ modules["pages/lesson/board"] = class {
     ".eAccount img": `width: 100%; height: 100%; object-fit: cover`,
     ".eLogin": `height: 32px; display: none; padding: 6px 10px; margin-left: 4px; background: var(--secondary); border-radius: 16px; color: #fff; font-size: 16px; font-weight: 600`,
 
+    ".eToolbarHolder": `position: relative; display: flex; gap: 8px; width: fit-content; flex: 1; margin: 8px 0; visibility: visible`,
+    ".eToolbar": `position: relative; display: flex; box-sizing: border-box; margin: auto 0; align-items: center; pointer-events: all`,
+    ".eViewerActions": `position: absolute; display: flex; height: calc(100% - 16px); left: 8px; top: 8px`,
+    ".eHandRaise": `position: relative; display: flex; box-sizing: border-box; width: 60px; height: 60px; margin: auto 4px; background: var(--pageColor); box-shadow: var(--lightShadow); pointer-events: all; --borderRadius: 30px`,
+    ".eHandRaise img": `position: absolute; width: 100%; height: 100%; left: 0px; top: 0px; border-radius: 30px`,
+    ".eHandRaise[selected]": `background: var(--theme)`,
+
+    ".eBottomHolder": `position: relative; width: 100%; height: 50px; margin-bottom: 8px; visibility: visible`,
+    ".eBottom": `position: absolute; display: flex; width: 100%; padding-top: 8px; left: 0px; top: 0px; justify-content: space-between; overflow-x: auto; scrollbar-width: none`,
+    ".eBottomHolder[scroll] .eBottom": `padding-top: 0px !important; background: var(--pageColor); box-shadow: var(--lightShadow); pointer-events: all`,
+    ".eBottom::-webkit-scrollbar": `display: none`,
+    ".eBottomScroll": `position: absolute; display: flex; width: 36px; height: 36px; top: 50%; transform: translateY(-50%); background: rgba(180, 218, 253, .75); opacity: 0; backdrop-filter: blur(2px); border-radius: 18px; justify-content: center; align-items: center; z-index: 200`,
+    ".eBottomHolder[scroll] .eBottomScroll": `opacity: 1 !important; pointer-events: all`,
+    ".eBottomScroll img": `width: 22px`,
+    ".eBottomScroll:active": `transform: translateY(-50%) scale(.85) !important`,
+    ".eBottomSection": `display: flex; box-sizing: border-box; height: 50px; padding: 6px; flex-shrink: 0; align-items: center; background: var(--pageColor); box-shadow: var(--lightShadow); pointer-events: all`,
+    ".eBottomHolder[scroll] .eBottomSection": `box-shadow: unset !important`,
+    ".eBottomSection[left]": `border-top-right-radius: 12px`,
+    ".eBottomSection[right]": `border-top-left-radius: 12px`,
+    ".ePageNav": `display: flex; width: 31px; height: 31px; margin: 0 4px; justify-content: center; align-items: center; background: var(--lightGray); border-radius: 16px`,
+    ".eCurrentPage": `margin: 0 6px; font-size: 20px; outline: unset`,
+    ".eCurrentPage:focus": `padding: 4px 12px; --borderWidth: 3px; --borderColor: var(--secondary); --borderRadius: 19px`,
   }; //".": ``,
   js = async (frame, extra) => {
     frame.style.position = "relative";

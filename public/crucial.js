d980b55fa044f063b73e4a7516915ca1055079bb
Exotek <exotekservices@gmail.com>
2025-01-12 21:46:16 -0600

    improved dropdown margin

--- public/crucial.js
+++ public/crucial.js
@@ -911,7 +911,7 @@ initSocket();
 // STANDARD MODULES //
 modules["dropdown"] = class {
   css = {
-    ".dropdown": `position: sticky; box-sizing: border-box; max-width: calc(100% - 16px); max-height: calc(100% - 16px); right: 0px; bottom: 0px; margin: 8px; opacity: 0; box-shadow: var(--shadow); border-radius: 12px; transform: scale(0); transform-origin: 0px 0px; pointer-events: all`,
+    ".dropdown": `position: sticky; box-sizing: border-box; max-width: calc(100% - 16px); max-height: calc(100% - 16px); right: 0px; bottom: 0px; margin: 12px; opacity: 0; box-shadow: var(--shadow); border-radius: 12px; transform: scale(0); transform-origin: 0px 0px; pointer-events: all`,
     ".dropdown .loading": `pointer-events: none`,
     ".dropdownOverflow": `position: relative; width: 100%; height: 100%; overflow: hidden; background: var(--pageColor); border-radius: inherit; z-index: 0`,
     ".dropdownContent": `position: absolute; box-sizing: border-box; width: max-content; max-width: var(--dropdownWidth); height: max-content; padding: 6px; overflow: auto`, //background: var(--pageColor)
@@ -961,7 +961,7 @@ modules["dropdown"] = class {
         addButtonWidth *= parseFloat(pageHolder.getAttribute("zoom"));
       }
       let setLeft = buttonRect.left + addButtonWidth - (dropdown.offsetWidth / 2);
-      let setTop = buttonRect.top - 4;
+      let setTop = buttonRect.top - 6;
       dropdown.style.left = setLeft + "px";
       dropdown.style.top = setTop + "px";
 

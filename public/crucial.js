9f77fdcda5c2965812d431b94d15a51aa8da6a69
Exotek <exotekservices@gmail.com>
2025-06-20 21:57:17 -0400

    updated version number?

--- public/crucial.js
+++ public/crucial.js
@@ -22,7 +22,7 @@ const configs = {
 };
 
 const config = configs["public"];
-const version = "1.3.7"; // Big Update . Small Feature Release . Bug Fix
+const version = "1.3.8"; // Big Update . Small Feature Release . Bug Fix
 
 const serverURL = config.server;
 const assetURL = config.assets;

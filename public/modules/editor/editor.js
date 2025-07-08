1378a02672c2f8c002b6fbec45543cac59a676e2
Exotek <exotekservices@gmail.com>
2025-07-08 12:07:50 -0500

    added user select none to images

--- public/modules/editor/editor.js
+++ public/modules/editor/editor.js
@@ -4845,7 +4845,7 @@ modules["editor/render/annotation/media"] = class extends modules["editor/render
 
   css = {
     ".eAnnotation[media]": `border-radius: 12px`,
-    ".eAnnotation[media] > img": `position: absolute; width: 100%; height: 100%; left: 0px; top: 0px; object-fit: cover; pointer-events: all; border-radius: inherit; transition: all .1s, border-radius 0s`,
+    ".eAnnotation[media] > img": `position: absolute; width: 100%; height: 100%; left: 0px; top: 0px; object-fit: cover; pointer-events: all; user-select: none; border-radius: inherit; transition: all .1s, border-radius 0s`,
     ".eAnnotation[media][border]:before": `content: ""; position: absolute; width: 100%; height: 100%; left: 0px; top: 0px; pointer-events: all; border-radius: inherit; background: var(--backgroundColor); box-shadow: inset 0px 0px 2px 0px var(--secondaryBackgroundColor)`,
     ".eAnnotation[media][border] > img": `width: calc(100% - 16px); height: calc(100% - 16px); left: 8px; top: 8px; border-radius: 4px`
   };

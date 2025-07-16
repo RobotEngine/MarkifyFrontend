6471ee7357fcd110a6546ed23db17071eda0abe4
HoustonHudgins <hwhudgins17@gmail.com>
2025-07-16 14:53:38 -0500

    login btn UI

--- public/modules/pages/launch.js
+++ public/modules/pages/launch.js
@@ -192,7 +192,7 @@ modules["pages/launch"] = class {
     ".lHeaderSummary": `max-width: 700px; margin-top: 24px; line-height: 26px`,
     ".lHeaderActions": `display: flex; flex-wrap: wrap; gap: 24px; margin-top: 40px; justify-content: center`,
 
-    ".lOpen": `display: flex; flex-direction: row; padding: 8px; bottom: 0px; align-items: center; --borderRadius: 20.25px; background: var(--theme); color: #fff;`,
+    ".lOpen": `display: flex; flex-direction: row; padding: 8px 14px 8px 8px; bottom: 0px; align-items: center; --borderRadius: 20.25px; background: var(--theme); color: #fff;`,
     ".lOpen img[loginBtnBeforeImg]": `width: 24px; height: 24px; margin-right: 8px; object-fit: cover; border-radius: 16px; border: 4px solid var(--themeColor); background: #0084FF;`,
     ".lOpen img[defaultLoginBtnBeforeImg]": `width: 24px; height: 24px; margin-right: 8px; object-fit: cover; border-radius: 16px; border: 4px solid #0084FF; background: #0084FF;`,
     ".lOpen img[loginBtnAfterImg]": `width: 24px; height: 24px; margin-left: 8px; filter: brightness(0) invert(1);`,
@@ -298,7 +298,7 @@ js = async (page) => {
     }
   } else {
     loginBtn.innerHTML = `<div accountuser>Open Markify</div>`;
-    loginBtn.style.padding = "8px 16px";
+    loginBtn.style.padding = "";
   }
 
 

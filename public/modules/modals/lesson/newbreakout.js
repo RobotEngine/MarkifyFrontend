076018fa22fd937a421bf18535cc5179505442db
Exotek <exotekservices@gmail.com>
2025-09-28 02:30:25 -0400

    changed breakout modal to use dots for progress

--- public/modules/modals/lesson/newbreakout.js
+++ public/modules/modals/lesson/newbreakout.js
@@ -1,5 +1,5 @@
 modules["modals/lesson/newbreakout"] = class {
-  maxHeight = 400;
+  //maxHeight = 400;
   preload = [
     "../modules/modals/lesson/newboard.js"
   ];
@@ -32,7 +32,11 @@ modules["modals/lesson/newbreakout"] = class {
         </div>
       </button>
     </div>
-    <div class="brtProgress">Step <b>1</b> of 3</div>
+    <div class="brtProgress">
+      <button class="largeButton" back hidden></button>
+      <div dots></div>
+      <button class="largeButton" next hidden></button>
+    </div>
   </div>
   `;
   css = {
@@ -47,7 +51,13 @@ modules["modals/lesson/newbreakout"] = class {
     ".brtButton div[image] svg": `width: 100%; height: 100%`,
     ".brtButton div[content]": `flex: 1; margin-left: 8px`,
     ".brtButton div[detail]": `margin-top: 4px; color: var(--textColor); font-size: 14px; font-weight: 500`,
-    ".brtProgress": `width: 100%; margin: 12px; font-size: 16px`
+    ".brtProgress": `display: flex; flex-wrap: wrap; gap: 8px; width: calc(100% - 24px); margin: 12px; font-size: 16px; justify-content: center; align-items: center`,
+    ".brtProgress .largeButton": `--themeColor: var(--theme); --themeColor2: var(--themeColor); --borderRadius: 10px; max-width: 100%; padding: 6px 10px; margin: 0; justify-content: center`,
+    ".brtProgress .largeButton svg": `width: 24px`,
+    ".brtProgress .largeButton[back] svg": `transform: scaleX(-1)`,
+    ".brtProgress div[dots]": `display: flex; flex-wrap: wrap; margin: 0 auto; justify-content: center; align-items: center`,
+    ".brtProgress div[dots] button": `display: flex; width: 16px; height: 16px; margin: 6px; background: var(--hover); border-radius: 8px`,
+    ".brtProgress div[dots] button[selected]": `width: 32px; background: var(--theme); pointer-events: none`
   };
   js = async (frame, extra) => {
     this.parent = extra.parent;
@@ -71,5 +81,15 @@ modules["modals/lesson/newbreakout"] = class {
     setSVG(blankButton.querySelector("div[image]"), "../images/editor/breakout/blank.svg");
     setSVG(cloneButton.querySelector("div[image]"), "../images/editor/breakout/clone.svg");
     setSVG(duplicateButton.querySelector("div[image]"), "../images/editor/breakout/duplicate.svg");
+
+
+    // Progess Bar:
+    let progressHolder = frame.querySelector(".brtProgress");
+    let backButton = progressHolder.querySelector("button[back]");
+    let progressDots = progressHolder.querySelector("div[dots]");
+    let nextButton = progressHolder.querySelector("button[next]");
+    setSVG(backButton, "../images/tooltips/arrow.svg");
+    progressDots.innerHTML = "<button selected></button><button></button><button></button>";
+    setSVG(nextButton, "../images/tooltips/arrow.svg");
   }
 }

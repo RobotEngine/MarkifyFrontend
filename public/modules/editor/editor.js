df79584de6ec1cbc2af771fe30ea228754e958be
Exotek <exotekservices@gmail.com>
2025-11-01 13:47:37 -0400

    added small pixel margin to equation box

--- public/modules/editor/editor.js
+++ public/modules/editor/editor.js
@@ -32,7 +32,7 @@ modules["editor/editor"] = class {
     ".eAnnotation:not([selected]) > .ql-container .ql-editor a": `pointer-events: none !important`,
     ".eAnnotation .ql-formula": `position: relative; border: none; background: unset !important; box-shadow: unset !important`,
     '.eAnnotation .ql-editor[contenteditable="false"] .ql-formula': `pointer-events: none !important`,
-    '.eAnnotation[selected] .ql-editor .ql-formula:before': `content: ""; position: absolute; box-sizing: border-box; width: calc(100% * var(--zoom)); height: calc(100% * var(--zoom)); left: 50%; top: 50%; transform: translate(-50%, -50%) scale(calc(1 / var(--zoom))); border: solid 2px var(--secondary); border-radius: 6px; opacity: 0; pointer-events: none; transition: opacity .15s`,
+    '.eAnnotation[selected] .ql-editor .ql-formula:before': `content: ""; position: absolute; box-sizing: border-box; width: calc((100% + 2px) * var(--zoom)); height: calc((100% + 2px) * var(--zoom)); left: 50%; top: 50%; transform: translate(-50%, -50%) scale(calc(1 / var(--zoom))); border: solid 2px var(--secondary); border-radius: 6px; opacity: 0; pointer-events: none; transition: opacity .15s`,
     '.eAnnotation[selected] .ql-editor[contenteditable="true"] .ql-formula:before': `opacity: 1`,
     
     //".ql-formula textarea": `transform: unset !important; clip: unset !important; width: unset !important; height: unset !important; outline: none !important`,

3ddccfa30c2ff4e96a23318142e811220a643db8
Exotek <exotekservices@gmail.com>
2025-12-16 11:23:12 -0600

    updated orientation to any

--- public/crucial.js
+++ public/crucial.js
@@ -22,7 +22,7 @@ const configs = {
 };
 
 const config = configs["public"];
-const version = "1.6.17"; // Big Update . Small Feature Release . Bug Fix
+const version = "1.6.18"; // Big Update . Small Feature Release . Bug Fix
 
 const serverURL = config.server;
 const assetURL = config.assets;

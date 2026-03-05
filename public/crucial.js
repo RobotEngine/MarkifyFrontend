7b17bad439e2d4d9d120ab5697015b68e0299985
Exotek <exotekservices@gmail.com>
2026-03-04 19:23:10 -0500

    fixed version and config

--- public/crucial.js
+++ public/crucial.js
@@ -21,8 +21,8 @@ const configurations = {
   }
 };
 
-const configuration = "prodTesting";
-const version = "1.6.58"; // Big Update . Small Feature Release . Bug Fix
+const configuration = "public";
+const version = "1.6.59"; // Big Update . Small Feature Release . Bug Fix
 const domain = "markifyapp.com";
 
 let config = configurations[configuration];

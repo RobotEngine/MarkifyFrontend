b70a294ff70928ad7cc3fbd067021474dcca6777
Exotek <exotekservices@gmail.com>
2026-05-22 17:39:10 -0500

    made quill import lowercase

--- src/modules/editor/imports.js
+++ src/modules/editor/imports.js
@@ -3,7 +3,7 @@ export const PDFJS = () => { return import("@/libraries/pdfjs/pdf.mjs"); };
 export const PDFJS_WORKER = () => { return import("@/libraries/pdfjs/pdf.worker.mjs"); };
 export const PDFJS_WORKER_PATH = () => { return import("@/libraries/pdfjs/pdf.worker.mjs?url"); };
 
-export const QUILL = () => { return import("Quill/core"); };
+export const QUILL = () => { return import("quill/core"); };
 
 export const MATHQUILL = () => { return import("@/libraries/mathquill/mathquill.min.js"); };
 
